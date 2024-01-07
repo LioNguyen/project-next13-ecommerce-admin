e678a742047e27e10cad387fe4d90bbc60a993cc
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-07 21:40:22 +0700

    feat: create form components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return null;
+};
+
+export default SetupPage;
