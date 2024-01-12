d96f55cc856180bfd0ce2aa5e429339eeb858304
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-12 16:58:44 +0700

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,18 @@ import { ModalProvider } from "@/providers/modal-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { ToastProvider } from "@/providers/toast-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Dashboard",
+export const metadata: Metadata = {
+  title: "Lio | E-Commerce Dashboard",
   description: "E-Commerce Dashboard",
+  icons: {
+    icon: "/ecommerce-logo.png",
+  },
 };
 
 export default function RootLayout({
